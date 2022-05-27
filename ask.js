2340d156231e1ca7c1ba398ab12ea3b5e5554948
Rebeca Sguerri <100148878+rebecasguerri@users.noreply.github.com>
2022-05-27 03:02:38 -0300

    12

--- ask.js
+++ ask.js
@@ -1,21 +1,19 @@
-const btns = document.getElementsByClassName('.box-ask-item');
-const respItens = document.getElementsByClassName('.box-resp');
+var btn = window.document.querySelectorAll('.box-ask-item');
+var btnAnsw = window.document.querySelectorAll('.box-resp');
 
-console.log(btns);
+console.log(btn)
+console.log(btnAnsw)
 
-/* btns[0].addEventListenerListener("click", () => console.log("ok"));
 
-for (var botao of btns){
-    botao.addEventListener("click", () => console.log("ok"));
-}
-//console.log(btns);
 
-//btns[0].addEventListener('click' , function() {
 
-  //  if(respItens.style.display === "block"){
-        respItens.style.display === "none"; 
- //   }else{
-        respItens.style.display === "block";
-  //  }
-   
-//});
+
+
+
+
+
+
+
+
+
+
