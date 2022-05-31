3b0ec8bd66409937aa2b012f7f6bc5f9d6cc7baf
Rebeca Sguerri <100148878+rebecasguerri@users.noreply.github.com>
2022-05-31 20:16:42 -0300

    final

--- ask.js
+++ ask.js
@@ -1,12 +1,92 @@
-var btn = window.document.querySelectorAll('.box-ask-item');
-var btnAnsw = window.document.querySelectorAll('.box-resp');
+//button1
+ const button1 = window.document.querySelector('#button1');
+ const btnAnsw1 = window.document.querySelector('#awsr1');
 
-console.log(btn)
-console.log(btnAnsw)
+     button1.addEventListener("click", () => {
+   
+     if( btnAnsw1.style.display ==="none"){
+            btnAnsw1.style.display="block"; 
+        }else{
+             btnAnsw1.style.display="none";
+        }
+   
+    });
 
 
+//button2
+ const button2 = window.document.querySelector('#button2');
+ const btnAnsw2 = window.document.querySelector('#awsr2');
+  
+    button2.addEventListener("click", () => {
+     
+     if( btnAnsw2.style.display ==="none"){
+            btnAnsw2.style.display="block"; 
+        }else{
+            btnAnsw2.style.display="none";
+        }
 
+    });
 
+  
+ //button3 
+ const button3 = window.document.querySelector('#button3');
+ const btnAnsw3 = window.document.querySelector('#awsr3');
+    
+    button3.addEventListener("click", () => {
+       
+     if( btnAnsw3.style.display ==="none"){
+            btnAnsw3.style.display="block"; 
+       }else{
+            btnAnsw3.style.display="none";
+       }
+       
+    });
+
+    
+//button4 
+ const button4 = window.document.querySelector('#button4');
+ const btnAnsw4 = window.document.querySelector('#awsr4');
+      
+    button4.addEventListener("click", () => {
+         
+     if( btnAnsw4.style.display ==="none"){
+            btnAnsw4.style.display="block"; 
+        }else{
+            btnAnsw4.style.display="none";
+        }
+          
+    });
+      
+
+//button5 
+ const button5 = window.document.querySelector('#button5');
+ const btnAnsw5 = window.document.querySelector('#awsr5');
+        
+    button5.addEventListener("click", () => {
+           
+     if( btnAnsw5.style.display ==="none"){
+            btnAnsw5.style.display="block"; 
+        }else{
+            btnAnsw5.style.display="none";
+        }
+            
+    });
+
+//button6  
+  const button6 = window.document.querySelector('#button6');
+  const btnAnsw6 = window.document.querySelector('#awsr6');
+          
+    button6.addEventListener("click", () => {
+             
+     if( btnAnsw6.style.display ==="none"){
+            btnAnsw6.style.display="block"; 
+        }else{
+            btnAnsw6.style.display="none";
+        }
+             
+    });
+             
+                    
 
 
 
